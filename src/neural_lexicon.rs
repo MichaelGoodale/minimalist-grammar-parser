dea47be3acf22919a87593d1a06395fae5797bf1
Michael Goodale <michael.goodale@mail.mcgill.ca>
2024-02-26 13:05:24 +0100

    Added meta-grammar

--- src/neural_lexicon.rs
+++ src/neural_lexicon.rs
@@ -1,48 +1,204 @@
-use std::any::Any;
-
 use crate::lexicon::{Feature, FeatureOrLemma, Lexiconable};
-use anyhow::{bail, Context, Result};
-use burn::tensor::{backend::Backend, ElementConversion, Tensor};
+use anyhow::Context;
+use burn::tensor::{activation::log_softmax, backend::Backend, Tensor};
 use logprob::LogProb;
 use petgraph::{graph::DiGraph, graph::NodeIndex, visit::EdgeRef};
 
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
 struct TensorPosition(usize);
 
-impl TryInto<TensorPosition> for &FeatureOrLemma<(), usize> {
-    fn try_into(self) -> Result<TensorPosition> {
-        Ok(TensorPosition(match self {
-            FeatureOrLemma::Lemma(_) => 0,
-            FeatureOrLemma::Feature(Feature::Category(_)) => 1,
-            FeatureOrLemma::Feature(Feature::Licensor(_)) => 2,
-            FeatureOrLemma::Feature(Feature::Licensee(_)) => 3,
-            FeatureOrLemma::Feature(Feature::Selector(_, crate::Direction::Left)) => 4,
-            FeatureOrLemma::Feature(Feature::Selector(_, crate::Direction::Right)) => 5,
-            _ => bail!("No conversion for root"),
-        }))
-    }
+const LEMMA_POS: TensorPosition = TensorPosition(0);
+const CATEGORY_POS: TensorPosition = TensorPosition(1);
+const LICENSOR_POS: TensorPosition = TensorPosition(2);
+const LICENSEE_POS: TensorPosition = TensorPosition(3);
+const LEFT_SELECTOR_POS: TensorPosition = TensorPosition(4);
+const RIGHT_SELECTOR_POS: TensorPosition = TensorPosition(5);
 
-    type Error = anyhow::Error;
-}
+type NeuralGraph<B> = DiGraph<(Option<Tensor<B, 1>>, FeatureOrLemma<(), usize>), ()>;
 
 #[derive(Debug)]
-struct NeuralLexicon<B: Backend> {
-    types: Tensor<B, 3>,      //(lexeme, lexeme_pos, type_distribution)
-    lemmas: Tensor<B, 3>,     //(lexeme, lexeme_pos, lemma_distribution)
-    categories: Tensor<B, 3>, //(lexeme, lexeme_pos, category_distribution)
-    graph: DiGraph<((usize, usize), FeatureOrLemma<(), usize>), ()>,
+pub struct NeuralLexicon<B: Backend> {
+    lemmas: Tensor<B, 3>, //(lexeme, lexeme_pos, lemma_distribution)
+    graph: NeuralGraph<B>,
     root: NodeIndex,
 }
 
+fn to_feature(pos: TensorPosition, category: Option<usize>) -> FeatureOrLemma<(), usize> {
+    match pos {
+        LEMMA_POS => FeatureOrLemma::Lemma(None),
+        CATEGORY_POS => FeatureOrLemma::Feature(Feature::Category(category.unwrap())),
+        LICENSEE_POS => FeatureOrLemma::Feature(Feature::Licensee(category.unwrap())),
+        LICENSOR_POS => FeatureOrLemma::Feature(Feature::Licensor(category.unwrap())),
+        LEFT_SELECTOR_POS => {
+            FeatureOrLemma::Feature(Feature::Selector(category.unwrap(), crate::Direction::Left))
+        }
+        RIGHT_SELECTOR_POS => FeatureOrLemma::Feature(Feature::Selector(
+            category.unwrap(),
+            crate::Direction::Right,
+        )),
+        _ => panic!("Invalid position!"),
+    }
+}
+
+fn get_prob_of_type_category<B: Backend>(
+    types: &Tensor<B, 3>,
+    categories: &Tensor<B, 3>,
+    category: usize,
+    type_position: TensorPosition,
+    n_lexemes: usize,
+    pos: usize,
+) -> Tensor<B, 1> {
+    let p_of_type: Tensor<B, 1> = types
+        .clone()
+        .slice([
+            0..n_lexemes,
+            pos..pos + 1,
+            type_position.0..type_position.0 + 1,
+        ])
+        .squeeze(1)
+        .sum_dim(0);
+
+    let p_of_category: Tensor<B, 1> = categories
+        .clone()
+        .slice([0..n_lexemes, pos..pos + 1, category..category + 1])
+        .squeeze(1)
+        .sum_dim(0);
+
+    p_of_type + p_of_category
+}
+
+fn get_prob_of_lemma<B: Backend>(
+    types: &Tensor<B, 3>,
+    n_lexemes: usize,
+    pos: usize,
+) -> Tensor<B, 1> {
+    let p_of_type: Tensor<B, 1> = types
+        .clone()
+        .slice([0..n_lexemes, pos..pos + 1, LEMMA_POS.0..LEMMA_POS.0 + 1])
+        .squeeze(1)
+        .sum_dim(0);
+
+    p_of_type
+}
+
+fn add_children<B: Backend>(
+    graph: &mut NeuralGraph<B>,
+    parent: NodeIndex,
+    pos: usize,
+    categories: &Tensor<B, 3>,
+    types: &Tensor<B, 3>,
+    n_lexemes: usize,
+) -> Vec<NodeIndex> {
+    let mut children = vec![];
+    for node_type in [LEFT_SELECTOR_POS, RIGHT_SELECTOR_POS, LICENSOR_POS] {
+        for category in 0..categories.shape().dims[2] {
+            let descendent: NodeIndex = graph.add_node((
+                Some(get_prob_of_type_category(
+                    types, categories, category, node_type, n_lexemes, pos,
+                )),
+                to_feature(node_type, Some(category)),
+            ));
+            graph.add_edge(parent, descendent, ());
+            children.push(descendent);
+        }
+    }
+    let lemma: NodeIndex = graph.add_node((
+        Some(get_prob_of_lemma(types, n_lexemes, pos)),
+        to_feature(LEMMA_POS, None),
+    ));
+    graph.add_edge(parent, lemma, ());
+    children.push(lemma);
+    children
+}
+
+fn add_til_category<B: Backend>(
+    graph: &mut NeuralGraph<B>,
+    parent: NodeIndex,
+    pos: usize,
+    categories: &Tensor<B, 3>,
+    types: &Tensor<B, 3>,
+    n_lexemes: usize,
+) -> (Vec<NodeIndex>, Vec<NodeIndex>) {
+    let mut licensee_children = vec![];
+    let mut category_children = vec![];
+    for node_type in [LICENSEE_POS, CATEGORY_POS] {
+        for category in 0..categories.shape().dims[2] {
+            let descendent: NodeIndex = graph.add_node((
+                Some(get_prob_of_type_category(
+                    types, categories, category, node_type, n_lexemes, pos,
+                )),
+                to_feature(node_type, Some(category)),
+            ));
+            graph.add_edge(parent, descendent, ());
+            match node_type {
+                LICENSEE_POS => licensee_children.push(descendent),
+                CATEGORY_POS => category_children.push(descendent),
+                _ => panic!("This is impossible to reach because of the preceeding array"),
+            }
+        }
+    }
+    (category_children, licensee_children)
+}
+
 impl<B: Backend> NeuralLexicon<B> {
-    fn new(
-        types: Tensor<B, 3>,
-        lemmas: Tensor<B, 3>,
-        categories: Tensor<B, 3>,
+    pub fn new(
+        types: Tensor<B, 3>,          //(lexeme, lexeme_pos, type_distribution)
+        lexeme_weights: Tensor<B, 1>, // (lexeme)
+        lemmas: Tensor<B, 3>,         //(lexeme, lexeme_pos, lemma_distribution)
+        categories: Tensor<B, 3>,     //(lexeme, lexeme_pos, categories_position)
     ) -> NeuralLexicon<B> {
         //TODO Implement function which uses types/lemmas/categories and gumbel softmax to make a
         //specific grammar
+        //
+        let n_lexemes = types.shape().dims[0];
+        let n_positions = types.shape().dims[1];
 
-        todo!();
+        let mut graph = DiGraph::new();
+        let root = graph.add_node((None, FeatureOrLemma::Root));
+        let lexeme_weights = log_softmax(lexeme_weights, 0).reshape([n_lexemes, 1, 1]);
+        let types = log_softmax(types, 2) + lexeme_weights.clone();
+        let categories = log_softmax(categories, 2) + lexeme_weights.clone();
+        let lemmas = log_softmax(lemmas, 2) + lexeme_weights;
+
+        let mut position = 0;
+
+        let (category_children, mut licensee_children) =
+            add_til_category(&mut graph, root, position, &categories, &types, n_lexemes);
+
+        let mut children = category_children;
+        let mut new_children = vec![];
+
+        let mut new_licensee_children = vec![];
+
+        while position < n_positions - 1 {
+            position += 1;
+
+            for child in children.drain(0..) {
+                new_children.extend(add_children(
+                    &mut graph,
+                    child,
+                    position,
+                    &categories,
+                    &types,
+                    n_lexemes,
+                ));
+            }
+            children.extend(new_children.drain(0..));
+
+            for child in licensee_children.drain(0..) {
+                let (category_children, licensee_children) =
+                    add_til_category(&mut graph, child, position, &categories, &types, n_lexemes);
+                new_licensee_children.extend(licensee_children.into_iter());
+                children.extend(category_children.into_iter());
+            }
+            licensee_children.extend(new_licensee_children.drain(0..));
+        }
+
+        NeuralLexicon {
+            lemmas,
+            graph,
+            root,
+        }
     }
 }
 
@@ -73,35 +229,8 @@ where
         &crate::lexicon::FeatureOrLemma<(), usize>,
         logprob::LogProb<f64>,
     )> {
-        if let Some(((lexeme, position), feature)) = self.graph.node_weight(nx) {
-            let type_position: TensorPosition = feature.try_into().unwrap();
-            let p: Tensor<B, 1> = self
-                .types
-                .clone()
-                .slice([
-                    *lexeme..lexeme + 1,
-                    *position..position + 1,
-                    type_position.0..type_position.0 + 1,
-                ])
-                .squeeze::<2>(0)
-                .squeeze::<1>(0);
-            let p: Tensor<B, 1> = {
-                match feature {
-                    FeatureOrLemma::Feature(f) => match f {
-                        Feature::Category(c)
-                        | Feature::Selector(c, _)
-                        | Feature::Licensor(c)
-                        | Feature::Licensee(c) => self
-                            .categories
-                            .clone()
-                            .slice([*lexeme..lexeme + 1, *position..position + 1, *c..c + 1])
-                            .squeeze::<2>(0)
-                            .squeeze::<1>(0),
-                    },
-                    _ => Tensor::ones_like(&p),
-                }
-            } * p;
-            let p: f64 = p.into_scalar().try_into().unwrap();
+        if let Some((Some(p), feature)) = self.graph.node_weight(nx) {
+            let p: f64 = p.clone().into_scalar().try_into().unwrap();
             Some((feature, LogProb::new(p).unwrap()))
         } else {
             None

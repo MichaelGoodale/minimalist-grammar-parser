fe238a438bd9b1626970a4a2601dbfe69d763d9f
Michael Goodale <michael.goodale@mail.mcgill.ca>
2024-02-26 17:38:34 +0100

    Fixed logsumexp

--- src/neural_lexicon.rs
+++ src/neural_lexicon.rs
@@ -65,8 +65,7 @@ fn get_prob_of_type_category<B: Backend>(
 }
 
 fn log_sum_exp<B: Backend>(a: Tensor<B, 1>) -> Tensor<B, 1> {
-    let max: Tensor<B, 1> = a.clone().max();
-    println!("{} {}", a, max);
+    let max: Tensor<B, 1> = a.clone().max().repeat(0, a.shape().dims[0]);
     (a - max.clone()).exp().sum().log() + max
 }
 

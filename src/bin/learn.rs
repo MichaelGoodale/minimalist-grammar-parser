cfd2ea7d87446b0eb2b87c741579f7f066fadd4d
Michael Goodale <michael.goodale@mail.mcgill.ca>
2024-03-06 15:35:33 +0100

    More progress on learner

--- src/bin/learn.rs
+++ src/bin/learn.rs
@@ -1,7 +1,14 @@
 use burn::{
     module::Module,
-    tensor::{backend::Backend, Tensor},
+    tensor::{
+        backend::{AutodiffBackend, Backend},
+        ops::IntTensor,
+        Int, Tensor,
+    },
+    train::{ClassificationOutput, TrainOutput, TrainStep},
 };
+use minimalist_grammar_parser::{get_neural_outputs, NeuralConfig};
+use rand::{thread_rng, Rng};
 
 #[derive(Module, Debug)]
 struct GrammarModel<B: Backend> {
@@ -11,9 +18,40 @@ struct GrammarModel<B: Backend> {
     weights: Tensor<B, 2>,
 }
 
-impl<B: Backend> GrammarModel<B> {
-    fn forward(&self, targets: Tensor<B, 2>) -> Tensor<B, 1> {
-        todo!();
+impl<B: Backend> GrammarModel<B>
+where
+    B::FloatElem: std::ops::Add<B::FloatElem, Output = B::FloatElem> + Into<f32>,
+{
+    fn forward(
+        &self,
+        targets: Tensor<B, 2, Int>,
+        neural_config: &NeuralConfig,
+        rng: &mut impl Rng,
+    ) -> Tensor<B, 1> {
+        get_neural_outputs(
+            self.lemmas.clone(),
+            self.types.clone(),
+            self.categories.clone(),
+            self.weights.clone(),
+            targets,
+            neural_config,
+            rng,
+        )
+    }
+}
+
+struct TargetStrings<'a, B: Backend> {
+    targets: Tensor<B, 2, Int>,
+    neural_config: &'a NeuralConfig,
+}
+
+impl<'a, B: AutodiffBackend> TrainStep<TargetStrings<'a, B>, Tensor<B, 1>> for GrammarModel<B>
+where
+    B::FloatElem: std::ops::Add<B::FloatElem, Output = B::FloatElem> + Into<f32>,
+{
+    fn step(&self, item: TargetStrings<B>) -> burn::train::TrainOutput<Tensor<B, 1>> {
+        let loss = self.forward(item.targets, item.neural_config, &mut rand::thread_rng());
+        TrainOutput::new(self, loss.backward(), loss)
     }
 }
 

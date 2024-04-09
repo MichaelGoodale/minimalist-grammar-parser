e83121880b5051cf6ccb125259a0bb88e15782fb
Michael Goodale <michael.goodale@mail.mcgill.ca>
2024-04-09 09:03:55 +0200

    flipped order

--- src/neural/neural_beam.rs
+++ src/neural/neural_beam.rs
@@ -225,13 +225,11 @@ impl Eq for NeuralBeam<'_> {}
 
 impl Ord for NeuralBeam<'_> {
     fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        match self
-            .generated_sentence
-            .len()
-            .cmp(&other.generated_sentence.len())
-        {
+        match self.max_log_prob.len().cmp(&other.max_log_prob) {
             std::cmp::Ordering::Less => std::cmp::Ordering::Less,
-            std::cmp::Ordering::Equal => (self.max_log_prob).cmp(&other.max_log_prob),
+            std::cmp::Ordering::Equal => {
+                (self.generated_sentence.len()).cmp(&other.generated_sentence.len())
+            }
             std::cmp::Ordering::Greater => std::cmp::Ordering::Greater,
         }
     }

317c715b23f34101469797e657be78e6f3a40289
Michael Goodale <michael.goodale@mail.mcgill.ca>
2024-04-12 14:27:18 +0200

    deatch only

--- src/neural/loss.rs
+++ src/neural/loss.rs
@@ -621,16 +621,12 @@ pub fn get_neural_outputs<B: Backend>(
     let (loss_per_grammar, grammar_losses, _, _, _, mut n_compatible) =
         get_grammar_losses(g, &lexicon, &alternatives, targets, neural_config)?;
 
-    let has_parse = n_compatible
-        .clone()
-        .mask_fill(n_compatible.clone().equal_elem(0.0).bool_not(), 1.0);
     //Probability of generating each of the target strings
     n_compatible = n_compatible
         .clone()
         .mask_fill(n_compatible.equal_elem(0.0), -0.5);
-    let loss: Tensor<B, 1> = ((loss_per_grammar.sum_dim(0) * has_parse.unsqueeze_dim(0))
-        + grammar_losses.clone().detach().unsqueeze_dim(0))
-    .squeeze(0);
+    let loss: Tensor<B, 1> =
+        (loss_per_grammar.sum_dim(0) + grammar_losses.clone().detach().unsqueeze_dim(0)).squeeze(0);
 
     Ok((
         -log_sum_exp_dim(loss, 0),

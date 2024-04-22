3781773929b015723e1d6db00ff943cb36414edc
Michael Goodale <michael.goodale@mail.mcgill.ca>
2024-04-22 16:55:43 +0200

    perhaps its this

--- src/neural/loss.rs
+++ src/neural/loss.rs
@@ -470,7 +470,7 @@ fn get_grammar_losses<B: Backend>(
         .sum_dim(2)
         .squeeze(2)
         .mask_fill(target_s_ids, -999.0);
-
+    /*
     let loss: Tensor<B, 2> = log_sum_exp_dim(
         Tensor::cat(
             vec![
@@ -481,7 +481,7 @@ fn get_grammar_losses<B: Backend>(
         ),
         2,
     )
-    .squeeze(2);
+    .squeeze(2);*/
 
     if grammar_splitting {
         let (grammar_probs, grammar_idx) = get_grammar_probs(string_probs, g, lexicon);

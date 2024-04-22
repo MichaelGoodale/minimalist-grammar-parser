fa53e521341367131f129265d6ae97708b522f39
Michael Goodale <michael.goodale@mail.mcgill.ca>
2024-04-22 18:36:33 +0200

    mean makes more sense

--- src/neural/loss.rs
+++ src/neural/loss.rs
@@ -630,7 +630,7 @@ pub fn get_neural_outputs<B: Backend>(
         true,
     );
 
-    let grammar = loss_per_grammar.sum_dim(0) + grammar_losses.unsqueeze_dim(0);
+    let grammar = loss_per_grammar.mean_dim(0) + grammar_losses.unsqueeze_dim(0);
 
     (
         -log_sum_exp_dim(grammar.clone(), 1).squeeze(1).sum_dim(0),

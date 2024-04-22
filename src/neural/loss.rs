e91219151361509171ea129b9db0ed0326ff8275
Michael Goodale <michael.goodale@mail.mcgill.ca>
2024-04-22 11:38:23 +0200

    hmm

--- src/neural/loss.rs
+++ src/neural/loss.rs
@@ -648,13 +648,13 @@ pub fn get_neural_outputs<B: Backend>(
 
     let grammar =
         loss_per_grammar.clone() + grammar_losses.clone().unsqueeze_dim(0) + string_probs.clone();
-    let compatible_loss: Tensor<B, 1> =
-        (string_probs.squeeze(0) + grammar_losses.clone()).select(0, idx.clone());
+    let compatible_loss: Tensor<B, 1> = softmax(string_probs.squeeze(0).select(0, idx.clone()), 0)
+        * grammar_losses.clone().select(0, idx.clone());
 
     (
         if max_n_compatible == 1.0 {
             -((log_sum_exp_dim(grammar.clone().select(1, idx), 1).squeeze(1)).mean_dim(0)
-                + compatible_loss.mean_dim(0))
+                + compatible_loss.sum_dim(0))
         } else {
             -(log_sum_exp_dim(grammar.clone().select(1, idx), 1).squeeze(1)).mean_dim(0)
         },

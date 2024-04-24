fe4b0d10626a680feb4d83e16948bb516b0e904b
Michael Goodale <michael.goodale@mail.mcgill.ca>
2024-04-24 14:48:38 +0200

    debug

--- src/neural/loss.rs
+++ src/neural/loss.rs
@@ -604,11 +604,10 @@ pub fn get_neural_outputs<B: Backend>(
         false,
     );
 
-    let p_of_t_given_p = log_sum_exp_dim(loss_per_grammar, 0).squeeze(0);
+    let p_of_t_given_p: Tensor<B, 1> = log_sum_exp_dim(loss_per_grammar, 0).squeeze(0);
     let p_of_p = string_probs + grammar_losses;
 
-    dbg!(n_compatible.clone());
     let n_compatible = n_compatible.sum_dim(1).squeeze(1);
     let n_compatible = Tensor::min_pair(Tensor::ones_like(&n_compatible), n_compatible);
-    (-log_sum_exp_dim(p_of_t_given_p + p_of_p, 0), n_compatible)
+    (-log_sum_exp_dim(p_of_p, 0), n_compatible)
 }

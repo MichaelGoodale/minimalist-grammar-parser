ef6933de5ea490ddd78fff4aa66c552f0f6c21e4
Michael Goodale <michael.goodale@mail.mcgill.ca>
2024-04-24 16:41:48 +0200

    debug

--- src/neural/loss.rs
+++ src/neural/loss.rs
@@ -591,7 +591,7 @@ pub fn get_neural_outputs<B: Backend>(
     target_vec: &[Vec<usize>],
     targets: Tensor<B, 2, Int>,
     neural_config: &NeuralConfig,
-) -> (Tensor<B, 3>, Tensor<B, 1>) {
+) -> (Tensor<B, 4>, Tensor<B, 1>) {
     let (loss_per_grammar, string_probs, grammar_losses, _, n_compatible) = get_grammar_losses(
         g,
         lexicon,
@@ -603,7 +603,7 @@ pub fn get_neural_outputs<B: Backend>(
         false,
     );
 
-    let p_of_t_given_p = log_sum_exp_dim(loss_per_grammar, 0).squeeze(0);
+    let p_of_t_given_p = log_sum_exp_dim(loss_per_grammar, 0);
     let p_of_p: Tensor<B, 1> = string_probs + grammar_losses;
 
     let n_compatible = n_compatible.sum_dim(1).squeeze(1);

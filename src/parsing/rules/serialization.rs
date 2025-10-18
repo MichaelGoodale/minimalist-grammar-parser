d5fbbfcc1d01e37d99cd9b530bec741edf032114
Michael Goodale <michael.goodale@mail.mcgill.ca>
2025-10-18 22:18:07 +0200

    Added petgraph exportation

--- src/parsing/rules/serialization.rs
+++ src/parsing/rules/serialization.rs
@@ -1,4 +1,5 @@
 use crate::{
+    Direction,
     lexicon::Feature,
     parsing::rules::{
         TraceId,
@@ -6,10 +7,12 @@ use crate::{
     },
 };
 use itertools::Itertools;
+use petgraph::graph::DiGraph;
 use serde::{
     Serialize,
     ser::{SerializeSeq, SerializeStructVariant},
 };
+use std::collections::VecDeque;
 use std::fmt::{Debug, Display};
 
 #[cfg(not(feature = "semantics"))]
@@ -25,6 +28,33 @@ use super::semantics::SemanticNode;
 pub struct Tree<'src, T, C: Eq + Display> {
     node: TreeNode<'src, T, C>,
     children: Vec<Tree<'src, T, C>>,
+}
+
+impl<'src, T: Clone, C: Clone + Eq + Display> Tree<'src, T, C> {
+    pub fn petgraph(&self) -> DiGraph<TreeNode<'src, T, C>, Direction> {
+        let mut g = DiGraph::new();
+        let root = g.add_node(self.node.clone());
+        let mut stack: VecDeque<_> = self
+            .children
+            .iter()
+            .enumerate()
+            .map(|(i, x)| (i, x, root))
+            .collect();
+        while let Some((pos, tree, par)) = stack.pop_front() {
+            let node = g.add_node(tree.node.clone());
+            g.add_edge(
+                par,
+                node,
+                match pos {
+                    0 => Direction::Left,
+                    1 => Direction::Right,
+                    _ => panic!("The library should only have binary branching!"),
+                },
+            );
+            stack.extend(tree.children.iter().enumerate().map(|(i, x)| (i, x, node)));
+        }
+        g
+    }
 }
 
 impl<'src, T, C: Eq + Display> Tree<'src, T, C> {
@@ -73,7 +103,7 @@ impl<'src, T: Display, C: Eq + Display> Tree<'src, T, C> {
 }
 
 #[derive(Debug, Clone, Eq, PartialEq, Hash)]
-struct TreeNode<'src, T, C: Eq + Display> {
+pub struct TreeNode<'src, T, C: Eq + Display> {
     node: MgNode<T, C>,
 
     storage: Storage<C>,
@@ -322,5 +352,29 @@ impl Serialize for TraceId {
         S: serde::Serializer,
     {
         self.0.serialize(serializer)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::grammars::STABLER2011;
+    use crate::{Lexicon, ParsingConfig};
+    use petgraph::dot::Dot;
+
+    #[test]
+    fn petgraph() -> anyhow::Result<()> {
+        let lex = Lexicon::from_string(STABLER2011)?;
+        let (_, _, r) = lex
+            .generate("C", &ParsingConfig::default())?
+            .next()
+            .unwrap();
+
+        let g = lex.derivation(r).tree().petgraph();
+
+        assert_eq!(
+            format!("{:?}", Dot::new(&g)),
+            "digraph {\n    0 [ label = \"TreeNode { node: Node { features: [Category(\\\"C\\\")] }, storage: Storage { h: {} }, semantics: None }\" ]\n    1 [ label = \"TreeNode { node: Leaf { lemma: Single(None), features: [Selector(\\\"V\\\", Right), Category(\\\"C\\\")] }, storage: Storage { h: {} }, semantics: None }\" ]\n    2 [ label = \"TreeNode { node: Node { features: [Category(\\\"V\\\")] }, storage: Storage { h: {} }, semantics: None }\" ]\n    3 [ label = \"TreeNode { node: Node { features: [Category(\\\"D\\\")] }, storage: Storage { h: {} }, semantics: None }\" ]\n    4 [ label = \"TreeNode { node: Node { features: [Selector(\\\"D\\\", Left), Category(\\\"V\\\")] }, storage: Storage { h: {} }, semantics: None }\" ]\n    5 [ label = \"TreeNode { node: Leaf { lemma: Single(Some(\\\"the\\\")), features: [Selector(\\\"N\\\", Right), Category(\\\"D\\\")] }, storage: Storage { h: {} }, semantics: None }\" ]\n    6 [ label = \"TreeNode { node: Leaf { lemma: Single(Some(\\\"king\\\")), features: [Category(\\\"N\\\")] }, storage: Storage { h: {} }, semantics: None }\" ]\n    7 [ label = \"TreeNode { node: Leaf { lemma: Single(Some(\\\"prefers\\\")), features: [Selector(\\\"D\\\", Right), Selector(\\\"D\\\", Left), Category(\\\"V\\\")] }, storage: Storage { h: {} }, semantics: None }\" ]\n    8 [ label = \"TreeNode { node: Node { features: [Category(\\\"D\\\")] }, storage: Storage { h: {} }, semantics: None }\" ]\n    9 [ label = \"TreeNode { node: Leaf { lemma: Single(Some(\\\"the\\\")), features: [Selector(\\\"N\\\", Right), Category(\\\"D\\\")] }, storage: Storage { h: {} }, semantics: None }\" ]\n    10 [ label = \"TreeNode { node: Leaf { lemma: Single(Some(\\\"beer\\\")), features: [Category(\\\"N\\\")] }, storage: Storage { h: {} }, semantics: None }\" ]\n    0 -> 1 [ label = \"Left\" ]\n    0 -> 2 [ label = \"Right\" ]\n    2 -> 3 [ label = \"Left\" ]\n    2 -> 4 [ label = \"Right\" ]\n    3 -> 5 [ label = \"Left\" ]\n    3 -> 6 [ label = \"Right\" ]\n    4 -> 7 [ label = \"Left\" ]\n    4 -> 8 [ label = \"Right\" ]\n    8 -> 9 [ label = \"Left\" ]\n    8 -> 10 [ label = \"Right\" ]\n}\n"
+        );
+        Ok(())
     }
 }

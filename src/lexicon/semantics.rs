a9ce9bb3176d21b19e668cf1c8d3f27972e60d86
Michael Goodale <michael.goodale@mail.mcgill.ca>
2025-10-31 15:07:02 +0100

    Working iota example

--- src/lexicon/semantics.rs
+++ src/lexicon/semantics.rs
@@ -567,4 +567,42 @@ John::0 -1::a_1";
         }
         Ok(())
     }
+
+    #[test]
+    fn iota_test() -> anyhow::Result<()> {
+        let grammar = [
+            "John::d -k::a_j",
+            "Mary::d -k::a_m",
+            "the::n= d -k::lambda <a,t> P iota(x, P(x))",
+            "vase::n::lambda a x pa_vase(x)",
+            "see::d= V::lambda e x pe_see(x)",
+            "::=>V +k agrO::lambda a x lambda e y PatientOf(x, y)",
+            "::=>agrO v::lambda <e,t> phi phi",
+            "::=>v =d voice::lambda a x lambda e y AgentOf(x, y)",
+            "s::=>voice +k t::lambda <e,t> P some_e(x, all_e, P(x) & Habitual#<e,t>(x))",
+        ]
+        .join("\n");
+
+        let lexicon = SemanticLexicon::parse(&grammar)?;
+        let s = [
+            PhonContent::Normal("John"),
+            PhonContent::Affixed(vec!["see", "s"]),
+            PhonContent::Normal("the"),
+            PhonContent::Normal("vase"),
+        ];
+        for (_, _, r) in lexicon
+            .lexicon
+            .parse(&s, "t", &ParsingConfig::default())
+            .unwrap()
+        {
+            for (pool, _) in r.to_interpretation(&lexicon) {
+                assert_eq!(
+                    pool.to_string(),
+                    "some_e(x, all_e, AgentOf(a_j, x) & PatientOf(iota(y, pa_vase(y)), x) & pe_see(x) & Habitual#<e,t>(x))"
+                )
+            }
+        }
+
+        Ok(())
+    }
 }

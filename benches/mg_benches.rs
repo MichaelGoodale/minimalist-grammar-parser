33cf27e407bb7bdf4cf58eebd9dee894f2f9923e
Michael Goodale <michael.goodale@mail.mcgill.ca>
2026-09-18 12:12:29 +0200

    Updated to latest version of simple_semantics

--- benches/mg_benches.rs
+++ benches/mg_benches.rs
@@ -173,13 +173,7 @@ runs::1= =1 =0 0::lambda a x lambda a y lambda t phi pa_man(a_Mary)
                 let interp = r
                     .to_interpretation(&semantics)
                     .take(10)
-                    .filter_map(|(pool, h)| {
-                        if let Ok(expr) = pool.into_pool() {
-                            Some((expr, h.into_rich(&semantics, &r)))
-                        } else {
-                            None
-                        }
-                    });
+                    .map(|(expr, h)| (expr, h.into_rich(&semantics, &r)));
                 parses.1.extend(interp);
             }
         }
@@ -188,15 +182,16 @@ runs::1= =1 =0 0::lambda a x lambda a y lambda t phi pa_man(a_Mary)
 
 #[divan::bench]
 fn semantic_bench(bencher: divan::Bencher) {
-    let bad_grammar="ε::0= 0::lambda t phi pa_man(a_John) | (~every_e(x, pe_likes, PatientOf(a_Phil, x)) & phi)
-ε::0= 0::lambda t phi every_e(x, pe_laughs, phi) & some(x, pa_man, (every(y, pa_woman, some(z, all_a, some(a, lambda a b some_e(c, pe_laughs, pe_laughs(c)), some(b, pa_woman, some_e(c, lambda e d pe_helps(c), PatientOf(b, c)) | ~(pa_man(a) | every(c, all_a, pa_woman(x))))))) & pa_woman(a_Phil) & some(y, all_a, every_e(z, pe_runs, every_e(a, pe_runs, PatientOf(a_Mary, a))))) | every(y, all_a, phi)) & some(x, all_a, some(y, all_a, some(z, lambda a a phi, some(a, all_a, every_e(b, pe_likes, pa_woman(x)))) & phi))
+    let bad_grammar="ε::0= 0::lambda t phi pa_man(a_John) | (~every_e(x, pe_likes(x), PatientOf(a_Phil, x)) & phi)
+ε::0= 0::lambda t phi every_e(x, pe_laughs(x), phi) & some(x, pa_man, (every(y, pa_woman, some(z, all_a, some(a, lambda a b some_e(c, pe_laughs(c), pe_laughs(c)), some(b, pa_woman, some_e(c, lambda e d pe_helps(c), PatientOf(b, c)) | ~(pa_man(a) | every(c, all_a, pa_woman(x))))))) & pa_woman(a_Phil) & some(y, all_a, every_e(z, pe_runs, every_e(a, pe_runs, PatientOf(a_Mary, a))))) | every(y, all_a, phi)) & some(x, all_a, some(y, all_a, some(z, lambda a a phi, some(a, all_a, every_e(b, pe_likes(b), pa_woman(x)))) & phi))
 laughs::0::(pa_woman(a_Sue) | every_e(x, pe_runs, ~pa_woman(a_Mary))) & pa_woman(a_Sue)
-sleeps::0::pa_woman(a_Phil) | ((every(x, lambda a y every_e(z, pe_likes, ~~pe_runs(z)), some(y, lambda a z pa_woman(y) & every_e(a, all_e, pe_runs(a)), ~(pa_woman(a_John) & ~every(z, all_a, ~((pa_man(z) & some_e(a, pe_runs, AgentOf(y, a)) & (pa_man(y) | ~pa_man(a_Phil) | ~pa_woman(x) | pa_man(a_Mary)) & (pa_man(a_John) | every_e(a, pe_helps, pe_loves(a))) & pa_man(a_John) & every(a, pa_woman, pa_man(a_Sue))) | (~some_e(a, all_e, pe_sleeps(a)) & every_e(a, pe_runs, some(b, all_a, PatientOf(a_Mary, a)) | pe_loves(a) | pe_loves(a) | pe_laughs(a)) & some_e(a, pe_sleeps, pe_loves(a)))))))) | ~~(~pa_man(a_John) | pa_woman(a_Mary) | pa_woman(a_Sue))) & pa_man(a_Sue) & ~every_e(x, pe_helps, (~some_e(y, pe_loves, some(z, pa_woman, pe_loves(x))) & pa_man(a_Mary) & pa_man(a_Phil)) | PatientOf(a_John, x)) & (every_e(x, lambda e y AgentOf(a_John, y), pe_sleeps(x)) | some(x, all_a, ~some_e(y, pe_likes, pe_sleeps(y) | pe_runs(y)))))
+sleeps::0::pa_woman(a_Phil) | ((every(x, lambda a y every_e(z, pe_likes(z), ~~pe_runs(z)), some(y, lambda a z pa_woman(y) & every_e(a, all_e, pe_runs(a)), ~(pa_woman(a_John) & ~every(z, all_a, ~((pa_man(z) & some_e(a, pe_runs, AgentOf(y, a)) & (pa_man(y) | ~pa_man(a_Phil) | ~pa_woman(x) | pa_man(a_Mary)) & (pa_man(a_John) | every_e(a, pe_helps, pe_loves(a))) & pa_man(a_John) & every(a, pa_woman, pa_man(a_Sue))) | (~some_e(a, all_e, pe_sleeps(a)) & every_e(a, pe_runs, some(b, all_a, PatientOf(a_Mary, a)) | pe_loves(a) | pe_loves(a) | pe_laughs(a)) & some_e(a, pe_sleeps, pe_loves(a)))))))) | ~~(~pa_man(a_John) | pa_woman(a_Mary) | pa_woman(a_Sue))) & pa_man(a_Sue) & ~every_e(x, pe_helps, (~some_e(y, pe_loves, some(z, pa_woman, pe_loves(x))) & pa_man(a_Mary) & pa_man(a_Phil)) | PatientOf(a_John, x)) & (every_e(x, lambda e y AgentOf(a_John, y), pe_sleeps(x)) | some(x, all_a, ~some_e(y, pe_likes(y), pe_sleeps(y) | pe_runs(y)))))
 Mary::0= 0::lambda t phi phi";
 
-    let fast_grammar = "ε::0= 0::lambda t phi (~every_e(x, pe_likes, PatientOf(a_Phil, x)) & phi)
-ε::0= 0::lambda t phi every_e(x, pe_laughs, phi)
-laughs::0::every_e(x, pe_runs, ~pa_woman(a_Mary)) & pa_woman(a_Sue)
+    let fast_grammar =
+        "ε::0= 0::lambda t phi (~every_e(x, pe_likes(x), PatientOf(a_Phil, x)) & phi)
+ε::0= 0::lambda t phi every_e(x, pe_laughs(x), phi)
+laughs::0::every_e(x, pe_runs(x), ~pa_woman(a_Mary)) & pa_woman(a_Sue)
 sleeps::0::pa_woman(a_Phil)
 Mary::0= 0::lambda t phi phi";
 
@@ -229,13 +224,7 @@ Mary::0= 0::lambda t phi phi";
                 let interp = r
                     .to_interpretation(&semantics)
                     .take(10)
-                    .filter_map(|(pool, h)| {
-                        if let Ok(expr) = pool.into_pool() {
-                            Some((expr, h.into_rich(&semantics, &r)))
-                        } else {
-                            None
-                        }
-                    });
+                    .map(|(expr, h)| (expr, h.into_rich(&semantics, &r)));
                 parses.1.extend(interp);
             }
         }
